da45b9955cd55dd2d9c9379514ab34af81ae0f06
Taras <tarasglprojects@gmail.com>
2023-04-06 19:37:44 +0300

    added handler for push events for sensors

--- src/main/java/com/smartfarm/sensordata/service/farm/model/SensorDTO.java
+++ src/main/java/com/smartfarm/sensordata/service/farm/model/SensorDTO.java
@@ -5,11 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class SensorDTO {
+public class SensorDTO implements Serializable {
 
     private String id;
     private String unitId;
